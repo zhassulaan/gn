9f03bece120b30892e9cfcfe0c90674dde03e2f8
Zhassulan Serikuly <zhass@MacBook-Air-Zhassulan.local>
2024-05-03 11:12:02 +0500

    header + body app

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <h2 class="header__text">Тестовое задание</h2>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  width: calc(100% - 24px * 2);
+  height: 56px;
+  margin: 24px 24px 16px;
+  padding: 16px 24px;
+  background-color: #FFF;
+  border: 1px solid #F0F2F5;
+  border-radius: 16px;
+}
+
+.header__text {
+  line-height: 24px;
+  font-size: 18px;
+  font-weight: 600;
+  color: #1D2739;
+}
+</style>
